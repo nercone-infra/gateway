73531993cde1148baa3f23c84d19b0bfbb902f7a
nercone-dev <nercone@nercone.dev>
2026-09-25 13:29:43 +0900

    --

--- onion/entrypoint.sh
+++ onion/entrypoint.sh
@@ -1,10 +1,23 @@
 #!/bin/sh
 set -e
 
-mkdir -p /var/lib/tor
-chown -R debian-tor:debian-tor /var/lib/tor
-chmod 700 /var/lib/tor
+main() {
+    mkdir -p /var/lib/tor
+    chown -R debian-tor:debian-tor /var/lib/tor
+    chmod 700 /var/lib/tor
 
-tor --verify-config -f /etc/tor.d/torrc
+    tor --verify-config -f /etc/tor.d/torrc
 
-exec tor -f /etc/tor.d/torrc
+    exec tor -f /etc/tor.d/torrc
+}
+
+reload() {
+    tor --verify-config -f /etc/tor.d/torrc
+    kill -HUP 1
+}
+
+case "${1:-main}" in
+    main)   main ;;
+    reload) reload ;;
+    *)      echo "Usage: $0 [main|reload]" >&2; exit 2 ;;
+esac
--- reload.sh
+++ reload.sh
@@ -7,4 +7,4 @@ sudo git pull
 
 docker compose exec proxy /usr/local/bin/entrypoint.sh reload
 
-docker compose kill -s HUP onion
+docker compose exec onion /usr/local/bin/entrypoint.sh reload
